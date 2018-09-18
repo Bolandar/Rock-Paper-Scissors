c91aac748b7ed1c553da38a4fd989fa6b084053d
Tim Rogers <timr36@comcast.net>
2018-09-17 19:57:22 -0700

    Added final scoring routine

--- RPS.py
+++ RPS.py
@@ -54,7 +54,7 @@ class HumanPlayer(Player):
 
     def move(self):
         while True:
-            choice = input("What is your move (rock, paper, or scissors)? ")
+            choice = input("  What is your move (rock, paper, or scissors)? ")
             if (choice.lower()) in moves:
                 return (choice.lower())
             else:
@@ -70,35 +70,44 @@ class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        p1_score = 0
-        p2_score = 0
+        self.p1_score = 0
+        self.p2_score = 0
 
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"{self.p1.name} picks: {move1}  {self.p2.name} picks: {move2}")
+        print(f"\n  {self.p1.name} picks: {move1}  {self.p2.name} picks: {move2}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
         self.show_winner(move1, move2)
 
     def show_winner(self, p1_move, p2_move):
         if beats(p1_move, p2_move) == True:
-            print(f"{p1_move} beats {p2_move}, {self.p1.name} wins!")
+            print(f"\n  {p1_move} beats {p2_move}, {self.p1.name} wins!\n")
+            self.p1_score += 1
         elif beats(p2_move, p1_move) == True:
-            print(f"{p2_move} beats {p1_move}, {self.p2.name} wins!")
+            print(f"\n  {p2_move} beats {p1_move}, {self.p2.name} wins!\n")
+            self.p2_score += 1
         else:
             if p1_move == p2_move:
-                print(f"Both players picked {p1_move}, game is a tie!")
+                print(f"\n  Both players picked {p1_move}, game is a tie!\n")
             else:
-                print("Wow, I'm not sure what happened")
+                print("  Wow, I'm not sure what happened")
 
 
     def play_game(self):
-        print("Game start!")
+        print("\n\n******************************************************")
+        print("***                                                ***")
+        print("***        Welcome to Rock, Paper, Scissors!       ***")
+        print("***                                                ***")
+        print("******************************************************\n\n")
         for round in range(3):
             print(f"Round {round}:")
+            print("------------------------------------------------------")
             self.play_round()
-        print("Game over!")
+        print("======================================================")
+        print(f"      Final Score:  {self.p1.name} - {self.p1_score}    {self.p2.name} - {self.p2_score}")
+        print("======================================================")
 
 
 if __name__ == '__main__':
